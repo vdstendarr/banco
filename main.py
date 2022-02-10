b5772f55a53cf983693904f30134943b3ee05ddd
vdstendarr <84213686+vdstendarr@users.noreply.github.com>
2022-02-10 18:32:11 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,41 +1,17 @@
-from PIL import Image
+from PIL import Image, ImageFont, ImageDraw 
 
+
+title_font = ImageFont.truetype('nevis.ttf', 50)
+
+W, H = (843,106)
 
 for i in range(201):
-    number = str(i).rjust(3, '0')
-    background = Image.open(r"bg.png")
-    
-    c = number[0]
-    d = number[1]
-    u = number[-1]
-
-    first_digit = Image.open(rf"numbers/{c}.png")
-    second_digit = Image.open(rf"numbers/{d}.png")
-    third_digit = Image.open(rf"numbers/{u}.png")
-
-    # Pasting img2 image on top of img1 
-    # starting at coordinates (0, 0)
-    posx_c = 43
-    posx_d = 57
-    posx_u = 71
-
-    if u == "1":
-        posx_u -= 4
-    if u == "4":
-        posx_u -= 2
-    
-    if d == "1":
-        posx_d -= 4
-        posx_u -= 4
-    if d == "4":
-        posx_d -= 2
-        posx_u -= 2
-
-    posy = 8 
-
-    background.paste(first_digit, (posx_c, posy), mask = first_digit)
-    background.paste(second_digit, (posx_d, posy), mask = second_digit)
-    background.paste(third_digit, (posx_u, posy), mask = third_digit)
-
-    # Displaying the image
-    background.save(f"results/{c}{d}{u}.png","PNG")
+    background = Image.open(r"background.png")
+    quantity = str(i)
+    image_editable = ImageDraw.Draw(background)
+    w, h = image_editable.textsize(quantity, font=title_font)
+    print(w,h)
+    image_editable.text(((W-w)/2,13), quantity, fill="white", font=title_font)
+    #image_editable.text((377,21), quantity, (255, 255, 255), font=title_font)
+    filename = f'{i}.png'
+    background.save(filename)
